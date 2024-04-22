79f50cac61280d2b3927e77820ef144d754038e9
ngentekis <ngentekis@gmail.com>
2024-04-21 19:12:27 -0500

    add config file, add todo service connection

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,19 +1,38 @@
 import { Injectable } from '@angular/core';
 import { Todo } from '../models/todo';
+import { Config } from '../../../config/config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  url = 'http://127.0.0.1:8000/api/todo/'
+  config: Config = require('../../../config/config.json');
+  url = this.config.apiUri;
   
   async getAllTodoItems(): Promise<Todo[]> {
-    const data = await fetch(`${this.url}get-todo`);
+    const data = await fetch(`${this.url}/get-todo`);
 
     const response = await data.json();
 
     return response ? response : [];
+  }
+
+  async addTodoItem(td_desc: String): Promise<void> {
+    let data = { td_desc: td_desc };
+
+    const response = await fetch(`${this.url}/add-todo`, {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)    
+    });
+
+    console.log(response.status);
+
   }
 
   constructor() { }
